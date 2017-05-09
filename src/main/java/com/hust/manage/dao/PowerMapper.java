46abe85c234433a1b337884a84e02703af4013ba
zhizhao <245442801@qq.com>
2017-05-09 17:21:28 +0800

    bulid dao.entity.xml in maven use mybatis generator

---
+++ src/main/java/com/hust/manage/dao/PowerMapper.java
@@ -0,0 +1,32 @@
+package com.hust.manage.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.hust.manage.model.Power;
+import com.hust.manage.model.PowerExample;
+
+public interface PowerMapper {
+    int countByExample(PowerExample example);
+
+    int deleteByExample(PowerExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Power record);
+
+    int insertSelective(Power record);
+
+    List<Power> selectByExample(PowerExample example);
+
+    Power selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Power record, @Param("example") PowerExample example);
+
+    int updateByExample(@Param("record") Power record, @Param("example") PowerExample example);
+
+    int updateByPrimaryKeySelective(Power record);
+
+    int updateByPrimaryKey(Power record);
+}
